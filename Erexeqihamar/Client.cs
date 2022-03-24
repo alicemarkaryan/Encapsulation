4460ec900c20cfdc347c67645a6414841aa38619
alicemarkaryan <alisa.markaryan@yahoo.com>
2022-03-24 21:14:32 +0400

    Add project files.

---
+++ Erexeqihamar/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erexeqihamar
+{
+    internal class Client
+    {
+       private string Name { get; set; }
+        private int Age { get; set; }
+       private string Gender { get; set; }
+
+      public Client(string gender, string name, int age)
+        {
+            this.Age=getAge();
+            this.Name = name;
+           this.Gender= getGender();       
+        }
+
+        public int getAge()
+        {
+            return Age; 
+        }
+        public void setAge(int age) 
+        {
+            if (age > 21 && age < 80)
+            {
+                Console.WriteLine(age);
+
+            }
+            else
+            {
+                Console.WriteLine("Age doesn't exist");
+            }
+        }
+        public string getGender()
+        {
+            return Gender;
+        }
+        public void setGender(string gender) {
+
+            if (gender.Equals("male"))
+            {
+                Console.WriteLine(gender);
+            }
+            else
+            {
+                Console.WriteLine("Gender doesn't exist");
+            }
+        }
+
+        public string getName(string name)
+        {
+            return name;
+        }
+
+
+
+
+    }
+}
---
+++ Erexeqihamar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erexeqihamar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client1 = new Client("male", "Ani", -55);
+            client1.setAge(-55);
+            client1.setGender("male");
+            Console.WriteLine(client1.getName("Ani"));
+        }
+    }
+}
